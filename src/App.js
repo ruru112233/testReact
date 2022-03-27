3f8883cba85cef7a0015ac02d98feb5471d8d185
ruru112233 <64506346+ruru112233@users.noreply.github.com>
2022-03-28 00:05:24 +0900

    ページ遷移

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Home } from "./components/Home";
+import { Page1 } from "./components/Page1";
+import { Page2 } from "./components/Page2";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Home />
+      <Page1 />
+      <Page2 />
     </div>
   );
 }
